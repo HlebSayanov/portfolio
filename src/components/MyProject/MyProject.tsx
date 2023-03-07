fb4771abd3ea53a81f64f3abbfd1f692473d695a
HlebSayanov <say4havi@gmail.com>
2023-03-08 00:11:38 +0300

    add_MyProjects_edit

--- src/components/MyProject/MyProject.tsx
+++ src/components/MyProject/MyProject.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
 import styleContainer from '../../common/styles/Container.module.css'
 import style from './MyProject.module.css'
+import {Project} from "./project/Project";
 export const MyProject = () => {
     return (
         <div className={style.myProjectBlock}>
             <div className={`${styleContainer.container} ${style.myProjectContainer}`}>
                 <h2>My Project</h2>
             <div className={style.projects}>
-                <div className={'project1'}>
-                    <div className={'icon&btn'}>
-                        <button>show</button>
-                    </div>
-                    <div className={'description'}>
-                        <h3>title</h3>
-                        <span>minimal descripriioon</span>
-                    </div>
-                </div>
-                <div className={'project2'}>
-                    <div className={'icon&btn'}>
-                        <button>show</button>
-                    </div>
-                    <div className={'description'}>
-                        <h3>title</h3>
-                        <span>minimal descripriioon</span>
-                    </div>
-                </div>
+                <Project/>
+
+
             </div>
             </div>
         </div>
